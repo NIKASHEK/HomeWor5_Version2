483abdd3f9ba11dfbe4b8e89a962cd2dbb85ecb7
NIKASHEK <nikashekiladze78@gmail.com>
2024-09-26 04:06:45 +0400

    Create README file and Complete first 4 task

---
+++ HomeWor5_Ver2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace HomeWor5_Ver2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                // Nika Shekiladze
+                // #1
+            //var x = Convert.ToInt32(Console.ReadLine());
+            //if (x % 5 == 0)
+            //    Console.WriteLine("YES");
+            //else
+            //    Console.WriteLine("NO");
+
+                // #2
+            //var x = Convert.ToDouble(Console.ReadLine());
+            //var y = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine();
+            //Console.WriteLine(x + y);
+            //if (x < y)
+            //    Console.WriteLine(y - x);
+            //else
+            //    Console.WriteLine(y - x);
+            //Console.WriteLine(x * y);
+            //if(y == 0)
+            //    Console.WriteLine("Not Allowed To Divide By Zero");
+            //else if (y > x)
+            //    Console.WriteLine(y / x);
+            //else 
+            //    Console.WriteLine(x / y);
+
+                // #3
+            //var x = Console.ReadLine();
+            //var y = Console.ReadLine();
+            //var someThing = x;
+            //x = y;
+            //y = someThing;
+            //Console.WriteLine();
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+                // #4
+            var x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine(x * i);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is HomeWork5 version 2
